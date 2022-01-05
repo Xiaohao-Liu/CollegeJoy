517d699ad4b4ba6c3fa16858dd58ea1072d38145
WYKXLDZ <32610353+WYKXLDZ@users.noreply.github.com>
2022-01-05 22:05:12 +0800

    Update dock.js

--- MacosDock/dock.js
+++ MacosDock/dock.js
@@ -1,6 +1,5 @@
 const default_option={
-    icon_size:40,
-    icon_padding:5,
+    icon_size:50,
     scale:1.5
 }
 
@@ -20,15 +19,23 @@ MacDock.prototype={
         this.dockdom.appendChild(this.coverdom);
         this.coverdom.setAttribute("class","cover")
         this.__add_items();
-        this.coverdom.onmousemove=(e)=>{
+        this.dockdom.onmousemove=(e)=>{
             this.__get_size(e.pageX)
         }
-        this.coverdom.onmouseleave=()=>{
+        this.dockdom.onmouseleave=()=>{
             this.reset_size();
         }
-        this.dockdom.style.width=this.items.length * (this.icon_size + this.icon_padding*2)+"px";
-        this.dockdom.style.height = (this.icon_padding*2 + this.icon_size) + "px";
+        this.compute_parameters()
+    },
+    compute_parameters:function(){
+        this.original_width = this.items.length * (this.icon_size );
+        this.dockdom.style.width= this.original_width+"px";
+        
+        this.dockdom.style.height = (this.icon_size) + "px";
         this.center();
+        this.original_left = this.dockdom.offsetLeft;
+        this.half_scaled_size = this.__get_size(this.dockdom.offsetLeft);
+        this.reset_size();
     },
     center:function(m){
         let domW = this.dockdom.offsetWidth;
@@ -40,7 +47,7 @@ MacDock.prototype={
     },
     __add_items:function(){
         let index = 0;
-        let left = this.icon_padding;
+        let left = 0;
         this.items.forEach(item => {
             const itemdom = document.createElement("div");
             this.coverdom.appendChild(itemdom);
@@ -51,44 +58,59 @@ MacDock.prototype={
             itemdomtitle.setAttribute("class","title");
             itemdomtitle.innerText=item.title;
             itemdomtitle.style.marginLeft=-(itemdomtitle.offsetWidth/2) + "px";
+            console.log(itemdomtitle)
             item.index = index;
             item.position = itemdom.offsetLeft + this.dockdom.offsetLeft;
             index += 1;
             item.dom = itemdom;
             item.dom.style.left = left + "px";
-            item.dom.style.bottom = this.icon_padding + "px";
+            item.dom.style.bottom = 0 + "px";
             item.dom.style.width = this.icon_size + "px";
             item.dom.style.height = this.icon_size + "px";
-            left += this.icon_size + this.icon_padding*2;
+            left += this.icon_size;
         });
     },
     __get_size:function(m){
-        this.coverdom.style.height=(this.icon_size * (this.scale + 1) + this.icon_padding*2) + "px";
+        let left_offset = 0
+        this.coverdom.style.height=(this.icon_size * (this.scale + 1) ) + "px";
         this.coverdom.style.marginTop=-this.icon_size *(this.scale) + "px";
-        let left = this.icon_padding;
-        this.items.forEach(item => {
-            item.position = item.dom.offsetLeft + this.dockdom.offsetLeft + .5*(this.icon_size + this.icon_padding*2);
-            let scale_computed = Math.floor(this.gussion((item.position-m)/(item.dom.offsetWidth + this.icon_padding*2)) * 100) / 100
-            let size = (scale_computed*this.scale + 1)*this.icon_size;
+        let left = 0;
+        this.items.forEach((item) => {
+            let scale_computed = Math.floor(this.gussion((item.position-m)/(this.icon_size)) * 1000) / 1000
+
+            let scale_size = (scale_computed*this.scale)*this.icon_size;
+            let size = scale_size + this.icon_size;
             item.dom.style.width = size + "px";
             item.dom.style.height = size + "px";
             
             item.dom.style.left = left + "px";
-            left += size + this.icon_padding*2;
+            left += size;
+            if(m-item.position<-this.icon_size/2){
+
+            }else if(m-item.position<this.icon_size/2){
+                left_offset += ((m - item.position )/this.icon_size + 0.5)*scale_size;
+            }
+            else if(m-item.position>=this.icon_size/2){
+                left_offset += scale_size;
+            }
+
         });
         this.dockdom.style.width=left+"px";
-        this.center(m);
+        this.dockdom.style.marginLeft=(-left_offset - this.original_width/2)+"px";
+        return left;
     },
     reset_size:function(){
-        let left = this.icon_padding;
+        let left = 0;
         this.items.forEach(item => {
             let size = this.icon_size;
             item.dom.style.width = size + "px";
             item.dom.style.height = size + "px";
             item.dom.style.left = left + "px";
-            left += this.icon_size + this.icon_padding*2;
+            item.position = this.original_left + left + this.icon_size*0.5;
+            left += this.icon_size;
         });
-        this.dockdom.style.width=this.items.length * (this.icon_size + this.icon_padding*2)+"px";
+        this.dockdom.style.width=this.items.length * (this.icon_size)+"px";
+        this.dockdom.style.height = this.icon_size + "px";
         this.coverdom.style.height="100%";
         this.coverdom.style.marginTop="0px";
         this.center();
